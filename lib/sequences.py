41412ddaa2dc19cacef12caec80aa75cf12fd6de
Charlie <cnewlinds@gmail.com>
2023-11-12 23:17:48 +1100

    test pass

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        my_fibonacci = []
+        print(my_fibonacci)
+    elif length == 1:
+        my_fibonacci = [0]
+        print(my_fibonacci) 
+    else:
+        my_fibonacci = [0, 1]
+        while len(my_fibonacci) < length:
+            num = my_fibonacci[-1] + my_fibonacci[-2]
+            my_fibonacci.append(num)
+        print(my_fibonacci)
+
+
+print_fibonacci(1)
